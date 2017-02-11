c41b6865f830c2bd2654f7a1e7ff9affae82378b
Danny <dannyfan@live.com>
2017-02-11 02:24:24 -0500

    Gets current user's city

--- clothes/views.py
+++ clothes/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
+import requests
+import json
 # Create your views here.
 
-
+def located():
+    address = requests.get('http://freegeoip.net/json/')
+    location = json.loads(address.text)
+    return location['city']
 
 
 def index(request):
-    return render(request, 'clothes/index.html')
+    location = located()
+    return render(request, 'clothes/index.html', {'location' : location})
