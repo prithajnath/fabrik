99f655d963befaf5ae5c13974ac48ed0830e4c3a
Danny <dannyfan@live.com>
2017-02-26 14:46:51 -0500

    changed allowed hosts

--- fabrik/settings.py
+++ fabrik/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4zcf(=*#b)lj$)^ovkiov0%!z2!1li48ppf%=w&*#4ze-$^9w1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com', 'fabrik-mparmelee.c9users.io']
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com', 'fabrik-mparmelee.c9users.io','https://sheltered-brook-70125.herokuapp.com/']
 
 
 # Application definition
