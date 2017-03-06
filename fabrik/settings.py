22d7e5261252c3f761c1be7a21f22083924aa224
Danny <dannyfan@live.com>
2017-03-06 01:53:50 -0500

    Fixing static error

--- fabrik/settings.py
+++ fabrik/settings.py
@@ -121,8 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+#STATIC_ROOT = ''
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
