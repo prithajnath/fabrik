d3dfae751d3cccadccdecd4cb92cddd2db89fc16
Prithaj <prithajnath@gmail.com>
2017-03-08 17:53:40 +0000

    Parse dic from Clarifai

--- clothes/views.py
+++ clothes/views.py
@@ -13,29 +13,10 @@ model = app.models.get('e0be3b9d6a454f0493ac3a30784001ff')
 #This function is the main part of this code, use it to format any clarifai API response
 def clarify(urlstr):
     x = app.inputs.create_image_from_filename(urlstr)
-    
-    predictions = str(model.predict([x]))
-    
-    #Outputing the response string to a file and formatting it
-    fil = open('output.txt', 'w')
-    for i in predictions:
-        if i == '}':
-            fil.write('}\n')
-        else:
-            fil.write(i)
-    
-    #Takes the highest confidence line
-    fil = open('output.txt','r')
-    for line in fil:
-        if (('\'id\': \'ai' in line) == True):
-            result = line
-            print(line)
-            break
-
-    #Takes only the name
-    result1 = result[(result.find('\'name\':')+7):]
-    result2 = result1[:result1.find(',')]
-    return result2
+    names = []
+    for i in model.predict([x])['outputs'][0]['data']['concepts']:
+        names.append(i['name'])
+    return names
 # Create your views here.
 
 def index(request):
