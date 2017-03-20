65dad7c9ca8c4cc30a39610a750abc9f43e31078
Danny <dannyfan@live.com>
2017-03-20 11:22:57 -0400

    logout view and url

--- user/urls.py
+++ user/urls.py
@@ -3,4 +3,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^login', auth_views.login, {'template_name' : 'user/login.html'}),
+    url(r'^logout/', auth_views.logout, {'template_name' : 'user/logout.html'}),
 ]
