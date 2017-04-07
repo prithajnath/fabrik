e0f40bf8c6c74acbdbaefb2231c3667ab721b900
Herdus <jason8541@gmail.com>
2017-04-07 11:16:33 -0400

    Actual clarifai integration?

--- clothes/views.py
+++ clothes/views.py
@@ -6,17 +6,18 @@ from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as ClImage
 from django.contrib.auth.decorators import login_required
 from fabrik.settings import LOGIN_REDIRECT_URL
+import base64
 
 #Jason's Clarifai Client ID, Client Secret
-'''
 app =  ClarifaiApp("wPXX8nSrRj_A25bqQAdAurGdZdbxzhJWELL9aaQ2","SRRlIyrXfV7bJkNE7R3naCBPkxw3J-UwqVgcxJhJ")
 #Model for Apparel Prediction
 model = app.models.get('e0be3b9d6a454f0493ac3a30784001ff')
 
 #Takes a URL in text, returns the response as a string
 #This function is the main part of this code, use it to format any clarifai API response
-def clarify(urlstr):
-    x = app.inputs.create_image_from_filename(urlstr)
+def clarify(file):
+    b64 = base64.encode(file)
+    x = app.inputs.create_image_from_base64(b64)
 
     predictions = str(model.predict([x]))
 
@@ -40,7 +41,7 @@ def clarify(urlstr):
     result1 = result[(result.find('\'name\':')+7):]
     result2 = result1[:result1.find(',')]
     return result2
-'''
+
 # Create your views here.
 
 @login_required(login_url=LOGIN_REDIRECT_URL)
@@ -61,8 +62,8 @@ def add(request):
             #form['clothing_type']=tag
             x = Clothing.objects.create(
                 name = name,
-                clothing_type = clothing_type,
                 image = image,
+                clothing_type = clarify(image),
                 owned_by = request.user
                 )
             form.save()
