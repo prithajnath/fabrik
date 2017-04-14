4dc266ae0bf0d3a7a5dca0f516da049da29edf3e
Herdus <jason8541@gmail.com>
2017-04-14 11:02:20 -0400

    Added fields for functional tags.

--- clothes/models.py
+++ clothes/models.py
@@ -8,6 +8,8 @@ class Clothing(models.Model):
     clothing_type = models.CharField(max_length=100)
     image = models.ImageField(upload_to='clothes/')
     owned_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True)
+    fWeather = models.CharField(max_length = 12)
+    fType = models.CharField(max_length = 12)
 
 
     def __str__(self):
