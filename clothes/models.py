8409a944dc2f46902bad5d94c0eeb55b39f8f819
Herdus <jason8541@gmail.com>
2017-04-17 11:34:19 -0400

    Update models.py

--- clothes/models.py
+++ clothes/models.py
@@ -8,8 +8,10 @@ class Clothing(models.Model):
     clothing_type = models.CharField(max_length=100)
     image = models.ImageField(upload_to='clothes/')
     owned_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True)
-    fWeather = models.CharField(max_length = 12)
-    fType = models.CharField(max_length = 12)
+    season = models.Charfield(max_length = 32)
+    
+    #fWeather = models.CharField(max_length = 12)
+    #fType = models.CharField(max_length = 12)
 
 
     def __str__(self):
